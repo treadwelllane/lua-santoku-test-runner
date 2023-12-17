999cd00227ac788ac61922d23dbd7fb69a4a3a3b
Matthew Brooks <mb964033@gmail.com>
2023-12-17 18:16:14 +0000

    update version of fs

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku-test-runner",
-  version = "0.0.4-1",
+  version = "0.0.5-1",
   variable_prefix = "TK_TEST_RUNNER",
   license = "MIT",
   public = true,
@@ -10,7 +10,7 @@ local env = {
     "lua >= 5.1",
     "santoku >= 0.0.148-1",
     "santoku-system >= 0.0.3-1",
-    "santoku-fs >= 0.0.6-1"
+    "santoku-fs >= 0.0.7-1"
   },
 
 }
