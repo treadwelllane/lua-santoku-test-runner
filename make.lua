0053475517bad5db821ce9996b45abbafae02635
Matthew Brooks <mb964033@gmail.com>
2024-02-07 23:49:02 +0000

    update deps

--- make.lua
+++ make.lua
@@ -1,16 +1,16 @@
 local env = {
 
   name = "santoku-test-runner",
-  version = "0.0.20-1",
+  version = "0.0.21-1",
   variable_prefix = "TK_TEST_RUNNER",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.179-1",
-    "santoku-system >= 0.0.19-1",
-    "santoku-fs >= 0.0.24-1"
+    "santoku >= 0.0.184-1",
+    "santoku-system >= 0.0.20-1",
+    "santoku-fs >= 0.0.25-1"
   },
 
   test = {}
